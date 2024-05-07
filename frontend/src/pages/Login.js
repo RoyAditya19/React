aa65469095edeb8165dbff84d817491de9fc9d92
Adii <royaditya1981@gmail.com>
2024-05-07 19:22:16 +0530

    day 2 commit

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <section>
+        <div className="container mx-auto p-4">
+            <div className="bg-white p-2 w-full max-w-md mx-auto ">
+                <div>
+                    <img src="" alt="Login Icon" />
+                </div>
+            </div>
+
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Login
